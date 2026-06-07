235a147d89bcbaa4de2706f26e20a4a1dbaede33
jagadeeswari-D <naaneynaan5@gamil.com>
2026-06-07 21:57:20 +0530

    progarm

---
+++ prog1.py
@@ -0,0 +1,10 @@
+a=int(input("enter a number: "))
+b=int(input("enter a 2nd number: "))
+c=int(input("enter  a 3rd number: "))
+if (a>b and a>c):
+    print("a is greater")
+elif(b>c):
+    print("b is  grearter")
+else:
+    print("c is greater")
+
