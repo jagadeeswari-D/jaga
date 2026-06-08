d65bdfbd0f60c6bb63df6b08949f963e0f90d1f1
JAGADEESWARI D <naaneynaan5@gmail.com>
2026-06-08 13:44:49 +0530

    push

--- prog8.py
+++ prog8.py
@@ -1,1 +1,16 @@
  #Discount Calculator – Apply discount based on purchase amount
+a=float(input("Enter the purchase amount: "))
+if a >= 1000:
+    discount = a * 0.1
+    print("You get a 10% discount of:", discount)
+    a-= discount
+    print("Purchase amount after discount:", a)
+elif a >= 500:
+    discount = a * 0.05
+    print("You get a 5% discount of:", discount)
+    a-= discount
+    print("Purchase amount after discount:", a)
+else:
+    discount = 0
+    print("No discount applicable.")
+
