e2487d59831f2a8f4220fb4fa781b6f9ed5cee31
JAGADEESWARI D <naaneynaan5@gmail.com>
2026-06-08 13:46:44 +0530

    push

--- progm12.py
+++ progm12.py
@@ -1,1 +1,10 @@
 #Find Second Largest Number – Find second largest among three numbers without sorting
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = int(input("Enter the third number: "))
+if (a > b) and (a < c) or (a < b) and (a > c):
+    print("Second largest number:", a)
+elif (b > a) and (b < c) or (b < a) and (b > c):
+    print("Second largest number:", b)
+else:
+    print("Second largest number:", c)
