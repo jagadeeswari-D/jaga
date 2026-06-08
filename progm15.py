c6e7151216d00d944d663c8821c0483287a8032b
JAGADEESWARI D <naaneynaan5@gmail.com>
2026-06-08 13:49:31 +0530

    push

--- progm15.py
+++ progm15.py
@@ -1,1 +1,11 @@
  #Yearly Bonus Calculator – Calculate employee bonus using experience
+years = int(input("Enter years of experience: "))
+if years < 2:
+    bonus = 0
+elif years < 5:
+    bonus = 2000
+elif years < 10:
+    bonus = 10000
+else:
+    bonus = 150000
+print("The yearly bonus is:", bonus)
