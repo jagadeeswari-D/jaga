353d46f0011b4d899d33052dd9186c5e34ec3000
JAGADEESWARI D <naaneynaan5@gmail.com>
2026-06-08 13:52:53 +0530

    push
    Implemented a number guessing game that provides hints.

--- progm17.py
+++ progm17.py
@@ -1,1 +1,12 @@
 # Number Guessing Game – Provide hints until the correct guess.
+import random
+n= random.randint(1, 100)
+while True:
+    g= int(input("Guess a number between 1 and 100: "))
+    if g < n:
+        print("Too low! Try again.")
+    elif g > n:
+        print("Too high! Try again.")
+    else:
+        print("Congratulations!!!! You guessed the number.")
+        break
